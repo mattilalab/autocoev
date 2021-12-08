2f674fa185f864e79139c7d00456bddf38371bf5
Petar Petrov <mattilalab@gmail.com>
2021-12-08 10:57:35 +0200

    Branch 0.1beta!

--- functions/results.sh
+++ functions/results.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Define paths and files
+#ETSV="$TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv"
+EOUT="$TMP/$RESULTS/filtered-P${PVALUE}-gaps${EGAPS}-pDiff${EPDIF}-Bonf${BONFERRONI}.tsv"
+
 # Inspect CAPS results. Separate pair folders where CAPS run failed,
 # skip pair folders where no coevolution was detected and copy those
 # where co-evolving pairs were found. To do this, first check if row
@@ -15,9 +19,12 @@ local pair="${1}"
       cp -a $pair $TMP/$RESULTS/fail
     elif [ "$numPairs" -eq 0 ]; then
       echo -e "[\e[34mNOCOEV\e[39m] No co-evolving pairs found for: $pair"
-      cp -a $pair $TMP/$RESULTS/nocoev
+      echo "$Seq1 $Seq2 $numPairs $totalComp $CutOff $thresholdR $averageR $averageSigR $tree1length $tree2length $gapThreshold $bootCutOff $DistanceCoef" >> $TMP/$RESULTS/coev_inter_nocoev.tsv
+      mkdir -p $TMP/$RESULTS/nocoev/$folder
+      cp -a $pair $TMP/$RESULTS/nocoev/$folder
     elif [ "$numPairs" -gt 0 ]; then
       echo -e "[\e[92mCOEVOL\e[39m] Copying pair with co-evolution: $pair"
+      echo "$Seq1 $Seq2 $numPairs $totalComp $CutOff $thresholdR $averageR $averageSigR $tree1length $tree2length $gapThreshold $bootCutOff $DistanceCoef" >> $TMP/$RESULTS/coev_inter_coev.tsv
       mkdir -p $TMP/$RESULTS/coev/$folder
       cp -a $pair $TMP/$RESULTS/coev/$folder
     else
@@ -50,6 +57,8 @@ results_cleanup() {
   done <<< $(echo "$SUMMARY")
 }
 
+# Save residue pairs with p-values below threshold and positive correlations
+# for both directions.
 adj_pVal(){
   local coevPair="${1}"
   sed 1d $coevPair/$coevPair.clean | while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 ; do
@@ -69,15 +78,15 @@ adj_pVal(){
     fi
   done
     
-  # Run Luqman's script
+  # Run Luqman's script for Bonferroni correction of p-values
   if [ -s $coevPair/$coevPair.$PVALUE.clean ]; then
     sed -i "1i msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2" $coevPair/$coevPair.$PVALUE.clean
     echo -e "Running R for Bonferroni correction for $coevPair"
     Rscript $CWD/R/AdjPval.R $coevPair/$coevPair.$PVALUE.clean $coevPair/$coevPair.$PVALUE.corrected
   elif [ -z $coevPair/$coevPair.$PVALUE.clean ]; then
-    echo -e "[SKIP] $coevPair.$PVALUE.clean not found. Skipping..."
+    echo -e "[SKIP] $coevPair.$PVALUE.clean is empty. Skipping..."
   else
-    echo -e "Check your input!"
+    echo -e "[SKIP] $coevPair.$PVALUE.clean does not exist. Skipping..."
   fi
 }
 
@@ -85,30 +94,37 @@ adj_pVal(){
 # Collect only pairs that exist in the reference organism (with "real" position > 0)
 extract_columns(){
   local coevPair="${1}"
-  sed 1d $coevPair/$coevPair.$PVALUE.corrected |
-  while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 bonferroni ; do
-    if (( $(echo "$bonferroni <= $BONFERRONI" |bc -l) && $(echo "$realA > 0" |bc -l) && $(echo "$realB > 0" |bc -l) )); then
-      seq1=$( sed "${realA}q;d" $TMP/$MSA/$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
-      seq2=$( sed "${realB}q;d" $TMP/$MSA/$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
-      flt1=$( sed "${realA}q;d" $TMP/$MSA/$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $3}' )
-      flt2=$( sed "${realB}q;d" $TMP/$MSA/$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $3}' )
-      echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $pvalA $pvalB $pMean $corr1 $corr2 $bonferroni" >> $coevPair/$coevPair.$PVALUE.corrected-$BONFERRONI
-      echo -e "Extracting $colA-$colB with corrected p-value=${bonferroni}"
-      mkdir -p $coevPair/columnStats-$BONFERRONI/$colA-$colB/
-      cat $coevPair/msa/$msa1.fa | seqkit subseq -r ${colA}:${colA} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colA-$msa1.txt
-      cat $coevPair/msa/$msa2.fa | seqkit subseq -r ${colB}:${colB} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colB-$msa2.txt
-     elif (( $(echo "$bonferroni > $BONFERRONI" |bc -l) )); then
-       echo -e "Skipping $colA-$colB with corrected p-value=${bonferroni}"
-     elif (( $(echo "$realA == 0" |bc -l) )); then
-       echo -e "Skipping $colA-$colB with MSA $colA -> $realA in $ORGANISM"
-     elif (( $(echo "$realB == 0" |bc -l) )); then
-       echo -e "Skipping $colA-$colB with MSA $colB -> $realB in $ORGANISM"
-     else
-       echo "Check settings!"
-     fi
-  done
+  if [ -s "$coevPair/${coevPair}.${PVALUE}.corrected" ]; then
+    sed 1d $coevPair/$coevPair.$PVALUE.corrected |
+    while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 bonferroni ; do
+      if (( $(echo "$bonferroni <= $BONFERRONI" |bc -l) && $(echo "$realA > 0" |bc -l) && $(echo "$realB > 0" |bc -l) )); then
+        seq1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+        seq2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+        flt1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $3}' )
+        flt2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $3}' )
+        echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $pvalA $pvalB $pMean $corr1 $corr2 $bonferroni" >> $coevPair/$coevPair.$PVALUE.corrected-$BONFERRONI
+        echo -e "Extracting $colA-$colB with corrected p-value=${bonferroni}"
+        mkdir -p $coevPair/columnStats-$BONFERRONI/$colA-$colB/
+        cat $coevPair/msa/$msa1.fa | seqkit subseq -r ${colA}:${colA} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colA-$msa1.txt
+        cat $coevPair/msa/$msa2.fa | seqkit subseq -r ${colB}:${colB} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colB-$msa2.txt
+       elif (( $(echo "$bonferroni > $BONFERRONI" |bc -l) )); then
+         echo -e "Skipping $colA-$colB with corrected p-value=${bonferroni}"
+       elif (( $(echo "$realA == 0" |bc -l) )); then
+         echo -e "Skipping $colA-$colB with MSA $colA -> $realA in $ORGANISM"
+       elif (( $(echo "$realB == 0" |bc -l) )); then
+         echo -e "Skipping $colA-$colB with MSA $colB -> $realB in $ORGANISM"
+       else
+         echo "Check settings!"
+       fi
+    done
+  else
+    echo "Skipping $coevPair.$PVALUE.corrected"
+  fi
 }
 
+# Calculate statistics on columns for the protein pairs with co-evolving
+# residues that passed the Bonferroni correction. First, check if file
+# exists and has non-zero size "if [ -s ... ]; then"
 columns_stats(){
   local coevPair="${1}"
   if [ -s "$coevPair/${coevPair}.${PVALUE}.corrected-${BONFERRONI}" ]; then
@@ -142,10 +158,25 @@ columns_stats(){
       # Calculate difference between raw p-values (absolute numbers)
       diffP=$(echo "${pvalA} - ${pvalB}" |bc -l | tr -d -)
       pDiff=$(printf "%1.10f" $diffP)
+
+
+      # Report total number of pairs
+      totNumP=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $3}')
+      
+      # Report total comparisons (MSA1*MSA2)
+      totComp=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $4}')
 	
       # Include correlation threshold as well. Just in case, make sure value is not in scientific format
       thrsR=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $6}')
       corrT=$(printf "%1.10f" $thrsR)
+      
+      # Include average correlation
+      averageR=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $7}')
+      averR=$(printf "%1.10f" $averageR)
+      
+      # Include average significant correlation
+      averageSigR=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $8}')
+      averSigR=$(printf "%1.10f" $averageSigR)
 	
       # Calculate coevolutionary correlation, normalized to threshold. For normalization:
       # Thanks to Dian Dimitrov.
@@ -153,23 +184,147 @@ columns_stats(){
       normC=$(echo "($corr - $corrT)/(1 - $corrT)" |bc -l)
       cCoev=$(printf "%1.10f" $normC)
       
-      echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $corrT $cCoev $pvalA $pvalB $pMean $pDiff $corr1 $corr2 $bonferroni $roundPerGapA $roundPerGapB $GapsAB $roundDivResA $roundDivResB $DivsAB" >> $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv
+      # Collect info of each amino acid pair into a singe file
+      echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $corrT $averR $averSigR $cCoev $pvalA $pvalB $pMean $pDiff $corr1 $corr2 $bonferroni $roundPerGapA $roundPerGapB $GapsAB $roundDivResA $roundDivResB $DivsAB $totNumP $totComp" >> $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv
 
-    done
+      # Convert Gblocks data (. or #) into numerical score. Collect data regardless of the Gblocks score.
+      if (( $(echo "$roundPerGapA > $EGAPS" |bc -l) && $(echo "$roundPerGapB > $EGAPS" |bc -l) && $(echo "$pDiff < $EPDIF" |bc -l) )); then
+        if [ "$flt1" = "#" ] && [ "$flt2" = "#" ]; then
+          echo -e "$msa1 <-> $msa2 with score 1"
+          echo "$msa1 $msa2 $cCoev $pMean $bonferroni $boot 1 $corr $corrT $averR $averSigR $totNumP $totComp" >> $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv
+        elif [ "$flt1" = "#" ] && [ "$flt2" = "." ]; then
+          echo -e "$msa1 <-> $msa2 with score 0.5"
+          echo "$msa1 $msa2 $cCoev $pMean $bonferroni $boot 0.5 $corr $corrT $averR $averSigR $totNumP $totComp" >> $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv
+        elif [ "$flt1" = "." ] && [ "$flt2" = "#" ]; then
+          echo -e "$msa1 <-> $msa2 with score 0.5"
+          echo "$msa1 $msa2 $cCoev $pMean $bonferroni $boot 0.5 $corr $corrT $averR $averSigR $totNumP $totComp" >> $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv
+        elif [ "$flt1" = "." ] && [ "$flt2" = "." ]; then
+          echo -e "$msa1 <-> $msa2 with score 0"
+          echo "$msa1 $msa2 $cCoev $pMean $bonferroni $boot 0 $corr $corrT $averR $averSigR $totNumP $totComp" >> $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv
+        else
+          echo "Something went wrong... CHECK!"
+        fi
+      else
+        # Make it echo sth else...
+        echo "No amino acid pairs passed the Bonferroni correction for $msa1 $msa2!"
+      fi      
+  done
+
+  # If no amino acid pairs passed the Bonferroni correction, prepare results
+  # for Chi^2 test as a protein pair with 0 coevolving amino acids.
+  elif [ ! -s "$coevPair/${coevPair}.${PVALUE}.corrected-${BONFERRONI}" ]; then
+    emsa_1=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $1}')
+    emsa_2=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $2}')
+    etotComp=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $4}')
+    echo "${emsa_1%.*} ${emsa_2%.*} 0 $etotComp" >> $TMP/$RESULTS/chi/proteinsFinal/${emsa_1%.*}.tsv
+    echo "${emsa_2%.*} ${emsa_1%.*} 0 $etotComp" >> $TMP/$RESULTS/chi/proteinsFinal/${emsa_2%.*}.tsv
+    echo "[NOPAIR] Prepare results for Chi_2 $coevPair.$PVALUE.corrected-${BONFERRONI}"
   else
-    echo "Skipping $coevPair.$PVALUE.corrected"
+    echo "[PASSED] Will analyze later: Chi_2 $coevPair.$PVALUE.corrected-${BONFERRONI}"
+  fi
+
+  # When column statistics are done, prepare results for Chi^2 test
+  if [ -s "$coevPair/summary-${PVALUE}-${BONFERRONI}.tsv" ]; then
+    msa_1=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $1}')
+    msa_2=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $2}')
+    coevNumber=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash count 1)
+    totCompar=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $13}')
+
+    echo "${msa_1} ${msa_2} $coevNumber $totCompar" >> $TMP/$RESULTS/chi/proteinsFinal/${msa_1}.tsv
+    echo "${msa_2} ${msa_1} $coevNumber $totCompar" >> $TMP/$RESULTS/chi/proteinsFinal/${msa_2}.tsv
+    echo "PICKING: $coevPair"
+  else
+    echo "SKIPPING: $coevPair"
   fi
 }
 
+# Export column statistics
+exp_column_stats() {
+  local coevPair="${1}"     
+  if [ -s "$coevPair/summary-${PVALUE}-${BONFERRONI}.tsv" ]; then
+           msa_1=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $1}')
+           msa_2=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $2}')
+  
+      coevNumber=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash count 1)
+
+        cCoevMIN=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+        cCoevMAX=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+       cCoevMEAN=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+       cCoevSDEV=$(awk '{print $3}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+       
+        pMeanMIN=$(awk '{print $4}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+        pMeanMAX=$(awk '{print $4}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+       pMeanMEAN=$(awk '{print $4}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+       pMeanSDEV=$(awk '{print $4}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+       
+   BonferroniMIN=$(awk '{print $5}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+   BonferroniMAX=$(awk '{print $5}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+  BonferroniMEAN=$(awk '{print $5}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+  BonferroniSDEV=$(awk '{print $5}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+
+         bootMIN=$(awk '{print $6}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+         bootMAX=$(awk '{print $6}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+        bootMEAN=$(awk '{print $6}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+	bootSDEV=$(awk '{print $6}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+  
+      gblocksMIN=$(awk '{print $7}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+      gblocksMAX=$(awk '{print $7}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+     gblocksMEAN=$(awk '{print $7}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+     gblocksSDEV=$(awk '{print $7}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+     
+         coevMIN=$(awk '{print $8}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash min 1)
+         coevMAX=$(awk '{print $8}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash max 1)
+        coevMEAN=$(awk '{print $8}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash mean 1)
+        coevSDEV=$(awk '{print $8}' $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | datamash sstdev 1)
+     
+         coevThr=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $9}')
+          avgCor=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $10}')
+      avgSignCor=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $11}')
+      coevNumAll=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $12}')
+       totCompar=$(head -n 1 $coevPair/summary-${PVALUE}-${BONFERRONI}.tsv | awk '{print $13}')
+
+      # Add score after Chi squared based on non-filtered results
+      if [ -s "$TMP/$RESULTS/chi/chi_test/${msa_1}.tsv" ] && [ -s "$TMP/$RESULTS/chi/chi_test/${msa_2}.tsv" ]; then
+        forward=$(grep "${msa_1} ${msa_2}" $TMP/$RESULTS/chi/chi_test/${msa_1}.tsv | awk '{print $7}')
+        reverse=$(grep "${msa_2} ${msa_1}" $TMP/$RESULTS/chi/chi_test/${msa_2}.tsv | awk '{print $7}')
+        chiboth=$(echo "$forward + $reverse" |bc -l)
+      else
+        echo "${msa_1} or ${msa_2} missing" >> $TMP/$RESULTS/miss.non-filtered
+      fi
+      
+      # Add score after Chi squared based on filtered results
+      if [ -s "$TMP/$RESULTS/chi/chi_test_final/${msa_1}.tsv" ] && [ -s "$TMP/$RESULTS/chi/chi_test_final/${msa_2}.tsv" ]; then
+	forward_fin=$(grep "${msa_1} ${msa_2}" $TMP/$RESULTS/chi/chi_test_final/${msa_1}.tsv | awk '{print $7}')
+	reverse_fin=$(grep "${msa_2} ${msa_1}" $TMP/$RESULTS/chi/chi_test_final/${msa_2}.tsv | awk '{print $7}')
+	chiboth_fin=$(echo "$forward_fin + $reverse_fin" |bc -l)
+      else
+        echo "${msa_1} or ${msa_2} missing" >> $TMP/$RESULTS/miss.filtered
+      fi
+      
+      # Collect data
+      echo "$msa_1 $msa_2 $coevThr $avgCor $avgSignCor $totCompar $coevNumAll $coevMIN $coevMAX $coevMEAN $coevSDEV $coevNumber $cCoevMIN $cCoevMAX $cCoevMEAN $cCoevSDEV $bootMIN $bootMAX $bootMEAN $bootSDEV $pMeanMIN $pMeanMAX $pMeanMEAN $pMeanSDEV $BonferroniMIN $BonferroniMAX $BonferroniMEAN $BonferroniSDEV $gblocksMIN $gblocksMAX $gblocksMEAN $gblocksSDEV $chiboth $chiboth_fin" >> $EOUT
+      echo "${msa_1} ${msa_2} added"
+      
+    else
+      echo "SKIPPING THE WHOLE PAIR: $coevPair"
+    fi
+}
+
+# Clean up the results, add column headers
 summary_cleanup(){
-  if [ -s $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv ]; then
+  if [ -s $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv ] && [ -s $TMP/$RESULTS/filtered-P${PVALUE}-gaps${EGAPS}-pDiff${EPDIF}-Bonf${BONFERRONI}.tsv ] ; then
     sed 1d $TMP/tsv/proteinsFound.tsv | while read -r idxml namexml attribute ; do
       sed -i "s/$idxml/$namexml $idxml/g" $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv
+      sed -i "s/$idxml/$namexml $idxml/g" $TMP/$RESULTS/filtered-P${PVALUE}-gaps${EGAPS}-pDiff${EPDIF}-Bonf${BONFERRONI}.tsv
     done
     sed -i \
-    "1i Name1 msa1 Name2 msa2 colA realA colB realB seq1 seq2 flt1 flt2 meanA meanB corr boot corrT cCoev pvalA pvalB pMean pDiff corr1 corr2 bonferroni GapA GapB GapAB DivA DivB DivAB" \
+    "1i Name1 msa1 Name2 msa2 colA realA colB realB seq1 seq2 flt1 flt2 meanA meanB corr boot corrT averR averSigR cCoev pvalA pvalB pMean pDiff corr1 corr2 bonferroni GapA GapB GapAB DivA DivB DivAB totNumP totComp" \
     $TMP/$RESULTS/pairs-P${PVALUE}-B${BONFERRONI}.tsv
+    sed -i \
+    "1i Name1 msa1 Name2 msa2 coevThr avgCor avgSignCor totCompar coevNumAll coevMIN coevMAX coevMEAN coevSDEV coevNumber cCoevMIN cCoevMAX cCoevMEAN cCoevSDEV bootMIN bootMAX bootMEAN bootSDEV pMeanMIN pMeanMAX pMeanMEAN pMeanSDEV BonferroniMIN BonferroniMAX BonferroniMEAN BonferroniSDEV gblocksMIN gblocksMAX gblocksMEAN gblocksSDEV chiboth chiboth_fin" \
+    $TMP/$RESULTS/filtered-P${PVALUE}-gaps${EGAPS}-pDiff${EPDIF}-Bonf${BONFERRONI}.tsv
   else
-    echo "No pairs-P${PVALUE}-B${BONFERRONI}.tsv !"
+    echo "No pairs-P${PVALUE}-B${BONFERRONI}.tsv or $TMP/$RESULTS/filtered-P${PVALUE}-gaps${EGAPS}-pDiff${EPDIF}-Bonf${BONFERRONI}.tsv!"
   fi
 }
+
