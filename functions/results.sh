55e454bb10b99c4eda587c46ab80f057fadd8376
Petar Petrov <mattilalab@gmail.com>
2022-01-21 16:37:17 +0200

    modified:   functions/results.sh

--- functions/results.sh
+++ functions/results.sh
@@ -157,79 +157,183 @@ results_cleanup() {
     fi
     done
     
-    #sed -i "1i msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2" bothWays.tsv
-    sed -i "1i msa1 msa2 colA realA colB realB corr boot p_value" bothWays.tsv
+  #sed -i "1i msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2" bothWays.tsv
+  sed -i "1i msa1 msa2 colA realA colB realB corr boot p_value" bothWays.tsv
     
-    # Run Luqman's script for Bonferroni & co correction of p-values
-    echo -e "Running R for Bonferroni correction for $coevPair"
-    Rscript $CWD/R/AdjPval.R bothWays.tsv bothWays-corrected.tsv
-    cd ..  
+  # Run Luqman's script for Bonferroni & co correction of p-values
+  echo -e "Running R for Bonferroni correction for $coevPair"
+  Rscript $CWD/R/AdjPval.R bothWays.tsv bothWays-corrected.tsv
+  cd ..
 }
 
-# Save residue pairs with p-values below threshold and positive correlations
-# for both directions.
-adj_pVal(){
+# get the amino acid from the reference organism's sequence and calculate
+# column statistics
+extract_columns_stats(){
   local coevPair="${1}"
-  sed 1d $coevPair/$coevPair.clean | while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 ; do
-     corrdec=$(printf "%1.10f" $corr)
-    corr1dec=$(printf "%1.10f" $corr1)
-    corr2dec=$(printf "%1.10f" $corr2)
-    pvalAdec=$(printf "%1.10f" $pvalA)
-    pvalBdec=$(printf "%1.10f" $pvalB)
-    pMeandec=$(printf "%1.10f" $pMean)
-    if (( $(echo "$pMeandec <= $PVALUE" |bc -l) && \
-    	  $(echo "$corr1dec > 0" |bc -l) && \
-	  $(echo "$corr2dec > 0" |bc -l) )); then
-      echo "Adding $pMeandec"
-      echo "$msa1 $msa2 $colA $realA $colB $realB $meanA $meanB $corrdec $boot $pvalAdec $pvalBdec $pMeandec $corr1dec $corr2dec" >> $coevPair/$coevPair.$PVALUE.clean
-    else
-      echo "Skipping $pMeandec"
-    fi
+  cd $coevPair
+  sed 1d bothWays-corrected.tsv | while read -r msa1 msa2 colA realA colB realB corr boot p_value bonferroni holm bh hochberg hommel by fdr; do
+    
+    # make sure the amino acid position exists in the reference organism
+    if (( $(echo "$realA > 0" |bc -l) && $(echo "$realB > 0" |bc -l) )); then
+      
+      # get amino acid and Gblocks score
+      seq1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+      seq2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+      flt1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $3}' )
+      flt2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $3}' )
+
+      ### Count the number of co-evolving sites from each protein
+      sitesCountA=$(awk '{print $4}' bothWays-corrected.tsv | datamash countunique 1)
+      sitesCountB=$(awk '{print $6}' bothWays-corrected.tsv | datamash countunique 1)
+
+      # Convert Gblocks scores to numbers (. = 0 ; # = 1)
+      if [ "$flt1" = "#" ]; then
+        gblscore1="1"
+      elif [ "$flt1" = "." ]; then
+        gblscore1="0"
+      else
+        echo "Check Gblocks scores!"
+      fi
+      if [ "$flt2" = "#" ]; then
+        gblscore2="1"
+      elif [ "$flt2" = "." ]; then
+        gblscore2="0"
+      else
+        echo "Check Gblocks scores!"
+      fi
+      
+      # Extract MSA columns for the co-evolving amino acids  
+      echo -e "Extracting $colA-$colB"
+      cat ./msa/$msa1.fa | seqkit subseq -r ${colA}:${colA} | seqkit sort -o columnStats/$msa1-$colA.txt
+      cat ./msa/$msa2.fa | seqkit subseq -r ${colB}:${colB} | seqkit sort -o columnStats/$msa2-$colB.txt
+
+      columnTotalA=$(sed '/^>/d' columnStats/$msa1-$colA.txt | wc -l)
+       columnGapsA=$(sed '/^>/d' columnStats/$msa1-$colA.txt | grep "-"  | wc -l)
+       roundPerGapA=$(printf "%1.5f" `echo "1 - ${columnGapsA}/${columnTotalA}" | bc -l`)
+      #roundPerGapA=$(printf "%1.5f" $percentageA)
+
+      columnNoGapsA=$(sed '/^>/d' columnStats/$msa1-$colA.txt | grep -v "-" | wc -l)
+      columnUniqueA=$(sed '/^>/d' columnStats/$msa1-$colA.txt | grep -v "-" | sort | uniq -c | sort -n -r | sed -n 1p | awk '{ print $1 }')
+      roundDivResA=$(printf "%1.5f" `echo "1 - ${columnUniqueA}/${columnNoGapsA}" | bc -l`)
+      #roundDivResA=$(printf "%1.5f" $diversityResA)
+
+      columnTotalB=$(sed '/^>/d' columnStats/$msa2-$colB.txt | wc -l)
+       columnGapsB=$(sed '/^>/d' columnStats/$msa2-$colB.txt | grep "-" | wc -l)
+       roundPerGapB=$(printf "%1.5f" `echo "1 - ${columnGapsB}/${columnTotalB}" | bc -l`)
+      #roundPerGapB=$(printf "%1.5f" $percentageB)
+
+      columnNoGapsB=$(sed '/^>/d' columnStats/$msa2-$colB.txt | grep -v "-" | wc -l)
+      columnUniqueB=$(sed '/^>/d' columnStats/$msa2-$colB.txt | grep -v "-" | sort | uniq -c | sort -n -r | sed -n 1p | awk '{ print $1 }')
+      roundDivResB=$(printf "%1.5f" `echo "1 - ${columnUniqueB}/${columnNoGapsB}" | bc -l`)
+       #roundDivResB=$(printf "%1.5f" $diversityResB)
+
+      # Calculate gaps and diversity    
+      GapsAB=$(printf "${roundPerGapA}\n $roundPerGapB" | datamash mean 1 )
+      DivsAB=$(printf "${roundDivResA}\n $roundDivResB" | datamash mean 1 )
+	 
+      # Calculate difference between raw p-values (absolute numbers)
+      #diffP=$(echo "${pvalA} - ${pvalB}" |bc -l | tr -d -)
+      #pDiff=$(printf "%1.10f" $diffP)
+
+      # Report total number of pairs
+      #totNumP=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $3}')
+      
+      ### Report total comparisons (MSA1*MSA2)
+      totComp=$(sed -n '2p' ${msa1}.fa_${msa2}.fa-coev_inter.csv | awk '{print $4}')
+	
+      # Include correlation threshold as well. Just in case, make sure value is not in scientific format
+      corrT1=$(printf "%1.10f" `sed -n '2p' ${msa1}.fa_${msa2}.fa-coev_inter.csv | awk '{print $6}'`)
+      corrT2=$(printf "%1.10f" `sed -n '2p' ${msa2}.fa_${msa1}.fa-coev_inter.csv | awk '{print $6}'`)
+       corrT=$(printf "${corrT1}\n $corrT2" | datamash mean 1 )
+      #corrT=$(printf "%1.10f" $thrsR)
+      
+      ### Include average correlation
+      #averageR=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $7}')
+      #averR=$(printf "%1.10f" $averageR)
+      
+      ### Include average significant correlation
+      #averageSigR=$(sed -n '2p' $coevPair/coev_inter.csv | awk '{print $8}')
+      #averSigR=$(printf "%1.10f" $averageSigR)
+	
+      # Calculate coevolutionary correlation, normalized to threshold. For normalization:
+      # https://www.mathworks.com/matlabcentral/answers/322438-normalize-data-with-a-threshold
+      # Thanks to Dian Dimitrov.
+      normC=$(printf "%1.10f" `echo "($corr - $corrT)/(1 - $corrT)" |bc -l`)
+      #cCoev=$(printf "%1.10f" $normC)
+      else
+      # Make it echo sth else...
+        echo "No amino acid pairs passed the Bonferroni correction for $msa1 $msa2!"
+      fi
+      echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $gblscore1 $gblscore2 $GapsAB $DivsAB $corrT $corr $normC $boot $p_value $bonferroni $holm $bh $hochberg $hommel $by $fdr" >> bothWays-corrected-columns.tsv
   done
-    
-  # Run Luqman's script for Bonferroni correction of p-values
-  if [ -s $coevPair/$coevPair.$PVALUE.clean ]; then
-    sed -i "1i msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2" $coevPair/$coevPair.$PVALUE.clean
-    echo -e "Running R for Bonferroni correction for $coevPair"
-    Rscript $CWD/R/AdjPval.R $coevPair/$coevPair.$PVALUE.clean $coevPair/$coevPair.$PVALUE.corrected
-  elif [ -z $coevPair/$coevPair.$PVALUE.clean ]; then
-    echo -e "[SKIP] $coevPair.$PVALUE.clean is empty. Skipping..."
-  else
-    echo -e "[SKIP] $coevPair.$PVALUE.clean does not exist. Skipping..."
-  fi
+  sed -i "1i msa1 msa2 colA realA colB realB seq1 seq2 gblscore1 gblscore2 GapsAB DivsAB corrT corr normC boot p_value bonferroni holm bh hochberg hommel by fdr" bothWays-corrected-columns.tsv
+cd ..
 }
 
-# Extract columns for pairs that passed the Bonferroni correction test.
-# Collect only pairs that exist in the reference organism (with "real" position > 0)
-extract_columns(){
-  local coevPair="${1}"
-  if [ -s "$coevPair/${coevPair}.${PVALUE}.corrected" ]; then
-    sed 1d $coevPair/$coevPair.$PVALUE.corrected |
-    while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 bonferroni ; do
-      if (( $(echo "$bonferroni <= $BONFERRONI" |bc -l) && $(echo "$realA > 0" |bc -l) && $(echo "$realB > 0" |bc -l) )); then
-        seq1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
-        seq2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
-        flt1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $3}' )
-        flt2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $3}' )
-        echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $pvalA $pvalB $pMean $corr1 $corr2 $bonferroni" >> $coevPair/$coevPair.$PVALUE.corrected-$BONFERRONI
-        echo -e "Extracting $colA-$colB with corrected p-value=${bonferroni}"
-        mkdir -p $coevPair/columnStats-$BONFERRONI/$colA-$colB/
-        cat $coevPair/msa/$msa1.fa | seqkit subseq -r ${colA}:${colA} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colA-$msa1.txt
-        cat $coevPair/msa/$msa2.fa | seqkit subseq -r ${colB}:${colB} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colB-$msa2.txt
-       elif (( $(echo "$bonferroni > $BONFERRONI" |bc -l) )); then
-         echo -e "Skipping $colA-$colB with corrected p-value=${bonferroni}"
-       elif (( $(echo "$realA == 0" |bc -l) )); then
-         echo -e "Skipping $colA-$colB with MSA $colA -> $realA in $ORGANISM"
-       elif (( $(echo "$realB == 0" |bc -l) )); then
-         echo -e "Skipping $colA-$colB with MSA $colB -> $realB in $ORGANISM"
-       else
-         echo "Check settings!"
-       fi
-    done
-  else
-    echo "Skipping $coevPair.$PVALUE.corrected"
-  fi
-}
+# # Save residue pairs with p-values below threshold and positive correlations
+# # for both directions.
+# adj_pVal(){
+#   local coevPair="${1}"
+#   sed 1d $coevPair/$coevPair.clean | while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 ; do
+#      corrdec=$(printf "%1.10f" $corr)
+#     corr1dec=$(printf "%1.10f" $corr1)
+#     corr2dec=$(printf "%1.10f" $corr2)
+#     pvalAdec=$(printf "%1.10f" $pvalA)
+#     pvalBdec=$(printf "%1.10f" $pvalB)
+#     pMeandec=$(printf "%1.10f" $pMean)
+#     if (( $(echo "$pMeandec <= $PVALUE" |bc -l) && \
+#     	  $(echo "$corr1dec > 0" |bc -l) && \
+# 	  $(echo "$corr2dec > 0" |bc -l) )); then
+#       echo "Adding $pMeandec"
+#       echo "$msa1 $msa2 $colA $realA $colB $realB $meanA $meanB $corrdec $boot $pvalAdec $pvalBdec $pMeandec $corr1dec $corr2dec" >> $coevPair/$coevPair.$PVALUE.clean
+#     else
+#       echo "Skipping $pMeandec"
+#     fi
+#   done
+#     
+#   # Run Luqman's script for Bonferroni correction of p-values
+#   if [ -s $coevPair/$coevPair.$PVALUE.clean ]; then
+#     sed -i "1i msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2" $coevPair/$coevPair.$PVALUE.clean
+#     echo -e "Running R for Bonferroni correction for $coevPair"
+#     Rscript $CWD/R/AdjPval.R $coevPair/$coevPair.$PVALUE.clean $coevPair/$coevPair.$PVALUE.corrected
+#   elif [ -z $coevPair/$coevPair.$PVALUE.clean ]; then
+#     echo -e "[SKIP] $coevPair.$PVALUE.clean is empty. Skipping..."
+#   else
+#     echo -e "[SKIP] $coevPair.$PVALUE.clean does not exist. Skipping..."
+#   fi
+# }
+
+# # Extract columns for pairs that passed the Bonferroni correction test.
+# # Collect only pairs that exist in the reference organism (with "real" position > 0)
+# extract_columns(){
+#   local coevPair="${1}"
+#   if [ -s "$coevPair/${coevPair}.${PVALUE}.corrected" ]; then
+#     sed 1d $coevPair/$coevPair.$PVALUE.corrected |
+#     while read -r msa1 msa2 colA realA colB realB meanA meanB corr boot pvalA pvalB pMean corr1 corr2 bonferroni ; do
+#       if (( $(echo "$bonferroni <= $BONFERRONI" |bc -l) && $(echo "$realA > 0" |bc -l) && $(echo "$realB > 0" |bc -l) )); then
+#         seq1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+#         seq2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $1$2}' )
+#         flt1=$( sed "${realA}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa1}.fa.${ORGANISM}.ref | awk '{print $3}' )
+#         flt2=$( sed "${realB}q;d" $TMP/$MSA/$GUIDANCEMSA-$GUIDANCECUT-$MSAMETHOD/${msa2}.fa.${ORGANISM}.ref | awk '{print $3}' )
+#         echo "$msa1 $msa2 $colA $realA $colB $realB $seq1 $seq2 $flt1 $flt2 $meanA $meanB $corr $boot $pvalA $pvalB $pMean $corr1 $corr2 $bonferroni" >> $coevPair/$coevPair.$PVALUE.corrected-$BONFERRONI
+#         echo -e "Extracting $colA-$colB with corrected p-value=${bonferroni}"
+#         mkdir -p $coevPair/columnStats-$BONFERRONI/$colA-$colB/
+#         cat $coevPair/msa/$msa1.fa | seqkit subseq -r ${colA}:${colA} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colA-$msa1.txt
+#         cat $coevPair/msa/$msa2.fa | seqkit subseq -r ${colB}:${colB} | seqkit sort -o $coevPair/columnStats-$BONFERRONI/$colA-$colB/$colB-$msa2.txt
+#        elif (( $(echo "$bonferroni > $BONFERRONI" |bc -l) )); then
+#          echo -e "Skipping $colA-$colB with corrected p-value=${bonferroni}"
+#        elif (( $(echo "$realA == 0" |bc -l) )); then
+#          echo -e "Skipping $colA-$colB with MSA $colA -> $realA in $ORGANISM"
+#        elif (( $(echo "$realB == 0" |bc -l) )); then
+#          echo -e "Skipping $colA-$colB with MSA $colB -> $realB in $ORGANISM"
+#        else
+#          echo "Check settings!"
+#        fi
+#     done
+#   else
+#     echo "Skipping $coevPair.$PVALUE.corrected"
+#   fi
+# }
 
 # Calculate statistics on columns for the protein pairs with co-evolving
 # residues that passed the Bonferroni correction. First, check if file
@@ -316,7 +420,7 @@ columns_stats(){
       else
         # Make it echo sth else...
         echo "No amino acid pairs passed the Bonferroni correction for $msa1 $msa2!"
-      fi      
+      fi
   done
 
   # If no amino acid pairs passed the Bonferroni correction, prepare results
